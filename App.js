94c02d85e38838fdb8c421832d08cc4141dcd8e1
Aarize <heyman955@gmail.com>
2022-10-29 12:55:36 +0530

    changes

--- App.js
+++ App.js
@@ -7,3 +7,5 @@ const items=[1,[2,[3,[4]]]]
 const newItems =_.flattenDeep(items);
 console.log(items);
 console.log(newItems);
+console.log("Hello Aarize Welcome to node JS tutorial")
+console.log("Hello Aarize Thors DSA bhi krlo")
